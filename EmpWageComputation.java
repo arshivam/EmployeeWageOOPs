84bb89dc9f0cc13e085943ffa4b6595a16ab8c92
arshivam <shivam.armarkar007@gmail.com>
2022-02-05 09:32:23 +0530

    Refactored the Code

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,27 +1,27 @@
 package com.bridgelabz.emoplyeewageoops;
 
 class EmpWage {
-	static final int ISPARTTIME = 1;
-	static final int ISFULLTIME = 2;
-	static final int WAGEPERHRS = 20;
-	static final int NOOFWORKINGDAYS = 20;
-	static final int MAXWORKINGHRS = 100;
+	static final int IS_PART_TIME = 1;
+	static final int IS_FULL_TIME = 2;
+	static final int WAGE_PER_HRS = 20;
+	static final int NO_OF_WORKINGDAYS = 20;
+	static final int MAX_WORKING_HRS = 100;
 
-	public void calculation() {
+	public void empWageCalculation() {
 		int totalEmpWage = 0, totalWorkingDays = 0, totalEmpHrs =0;
 
-		while (totalEmpHrs <= MAXWORKINGHRS &&
-				totalWorkingDays < NOOFWORKINGDAYS)
+		while (totalEmpHrs <= MAX_WORKING_HRS &&
+				totalWorkingDays < NO_OF_WORKINGDAYS)
 		{
 			int empHrs = 0;
 			totalWorkingDays++;
 			int empcheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empcheck)
 			{
-			case ISPARTTIME:
+			case IS_PART_TIME:
 				empHrs = 4;
 				break;
-			case ISFULLTIME:
+			case IS_FULL_TIME:
 				empHrs = 8;
 				break;
 			default:
@@ -29,7 +29,7 @@ class EmpWage {
 				break;
 			}
 			totalEmpHrs += empHrs;
-			int empWage =empHrs * WAGEPERHRS;
+			int empWage =empHrs * WAGE_PER_HRS;
 			totalEmpWage += empWage;
 		}
 		System.out.println("Total Employee Wage :"+ totalEmpWage);
@@ -39,6 +39,6 @@ class EmpWage {
 public class EmpWageComputation {
 	public static void main(String[] args) {
 		EmpWage a = new EmpWage();
-		a.calculation();	
+		a.empWageCalculation();	
 	}
 }
